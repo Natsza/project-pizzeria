3233f7eacadac589cd5aa2bbb83c98de45de3054
Natalia Szymanska <natsza.ns@gmail.com>
2023-08-11 19:14:33 +0200

    Add initAccordion method

--- dist/js/script.js
+++ dist/js/script.js
@@ -62,6 +62,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new product: ', thisProduct);
     }
@@ -79,6 +80,34 @@
 
       /* add element to menu*/
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProducts = document.querySelectorAll(
+          select.all.menuProductsActive
+        );
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        for (let activeProduct of activeProducts) {
+          if (activeProduct != thisProduct.element) {
+            activeProduct.classList.remove('active');
+          }
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
     }
   }
 
